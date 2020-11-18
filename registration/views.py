1b76e96f4883745c2e2fa41e14ae1a29757fa973
Anthony Luque <kasuru@hotmail.com>
2020-11-17 19:21:03 -0500

    [ADD] home template, urls and view

--- registration/views.py
+++ registration/views.py
@@ -1,6 +1,19 @@
+from django.shortcuts import render
+
+from django.views.generic.base import TemplateView
 from django.views.generic import CreateView
 
 # Create your views here.
 
 class SignUpView(CreateView):
     pass
+
+
+# Create your views here.
+
+class HomePageView(TemplateView):
+
+    template_name = 'registration/home.html'
+
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name, {'title':'Mi Super Web'})
